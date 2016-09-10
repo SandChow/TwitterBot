c4aaf4476ae0e101ed9ff8e8c91f0d6cf6b79372
SandChow <srisandeep@hotmail.com>
2016-09-10 14:32:36 -0700

    Add the main bot script

---
+++ bot.py
@@ -0,0 +1,12 @@
+#!/Users/Sandeep/miniconda2/envs/ML
+import json
+import requests
+import tweepy
+
+def main():
+    # Twitter API setup
+    with open(SECRETS_FILE) as f:
+        secrets = json.load(f)
+    auth = tweepy.OAuthHandler(secrets['consumer_key'], secrets['consumer_secret'])
+    auth.set_access_token(secrets['access_token'], secrets['access_secret'])
+    api = tweepy.API(auth)
