e20f463ee11861e0a528448760387b11a9b67edd
SandChow <srisandeep@hotmail.com>
2016-09-13 20:58:13 -0700

    Remove bugs with stream

--- bot.py
+++ bot.py
@@ -2,18 +2,16 @@
 import json
 import requests
 import tweepy
-from stream import MyStreamListener
+from stream import StreamListener
 
 SECRETS_FILE = "secrets.json"
 
 def main():
 	api = authorize()
-	myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
-	for tweet in myStream.filter(track=['soccer'], locations=[-122.75,36.8,-121.75,37.8,-74,40,-73,41]):
-		if not tweet['retweeted'] and 'RT @' not in tweet['text']:
-			if 'soccer' in tweet['text']:
-				print tweet
-				
+	stream = tweepy.Stream(auth = api.auth, listener=StreamListener())
+	for tweet in stream.filter(track=['soccer', 'Soccer', 'SOCCER']):
+		print tweet
+
 def authorize():
     # Twitter API setup
     with open(SECRETS_FILE) as f:
