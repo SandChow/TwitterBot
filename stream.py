5634cfbf23f0403ce03f6e41d6f33c11d48e8dd5
SandChow <srisandeep@hotmail.com>
2016-09-13 20:56:30 -0700

    Update to limit to tweets with 'soccer'

--- stream.py
+++ stream.py
@@ -1,8 +1,9 @@
 import tweepy
 import re
 #override tweepy.StreamListener to add logic to on_status
-class MyStreamListener(tweepy.StreamListener):
+class StreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
     	if not re.search('^RT', status.text):
-    		print status.text
+    		if 'soccer' in status.text:
+				print status.text
